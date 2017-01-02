22a510a80c9e1934aa8ebd63426ea50b8ca93520
rohit rathi <rohit.rathi@green-apex.com>
2017-01-02 21:07:51 +0530

    CRUD method definedclear

--- src/main/java/com/ga/controller/UserController.java
+++ src/main/java/com/ga/controller/UserController.java
@@ -3,9 +3,12 @@ package com.ga.controller;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -41,4 +44,30 @@ public class UserController {
 
         return userService.getAllUser();
     }
+    
+    @GET
+    @Path("/{userID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User checkUserToUpdate(@PathParam("userID") int id) {
+
+        return userService.checkUserToUpdate(id);
+    }
+    
+    @DELETE
+    @Path("delete/{userID}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String checkUserToDelete(@PathParam("userID") int id){
+
+        return userService.checkUserToDelete(id);
+    }
+    
+    @POST
+    @Path("update/{userID}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String updateUser(@PathParam("userID") int id,User user){
+
+        return userService.updateUser(id,user);
+    }
+    
 }
